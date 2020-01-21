454debe9e708ee3ed182664adcd7d579c517200b
ShivamKumarJha <jha.shivam3@gmail.com>
2020-01-21 05:29:59 +0000

    s1067e_1g-user 8.1.0 OPM2.171019.012 40410 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/dexp/VA110
+BOARD_VENDOR := dexp
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/dexp/VA110/BoardConfigVendor.mk
